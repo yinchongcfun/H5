5ad24611c575245684c46d2c3239961ac2a3ac9c
cfun <1606548133@qq.com>
2019-09-02 18:10:32 +0800

    H5提交

---
+++ src/main/java/com/yc/h5/H5Application.java
@@ -0,0 +1,14 @@
+package com.yc.h5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class H5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H5Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/yc/h5/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.yc.h5.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
+
